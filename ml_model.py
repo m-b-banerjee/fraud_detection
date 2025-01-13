f94019bb9b9aff0702ec684d52c700de518f450c
m-b-banerjee <monalisa.banerjee.edu@gmail.com>
2025-01-13 20:03:01 +0100

    added 3 new ML model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,17 @@
 
 
 print("adding all ML related Code here")
+
+def model1():
+    print("adding code for ML Model1")
+
+
+
+def model2():
+    print("adding code for ML Model2")
+
+
+
+
+def model3():
+    print("adding code for ML Model3")
